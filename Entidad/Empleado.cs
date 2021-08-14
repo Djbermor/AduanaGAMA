274b9023a8c74ba65d0953e90594ec57506dc48f
Djbermor <deividber@hotmail.com>
2021-08-14 03:46:41 -0500

    creacion de proyecto entidad para pasar los datos del empleado

---
+++ Entidad/Empleado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    public class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Salario { get; set; }
+        public string Departamento { get; set; }
+        public string Rol { get; set; }
+        public string Fecha { get; set; }
+        public string Sexo { get; set; }
+        public string CodigoEmpresa { get; set; }
+    }
+}
